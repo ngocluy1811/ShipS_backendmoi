ca8d74faec98367d6dc1b44e9cca6a25ffcb6032
ngocluydepzai <vongocluy12345@gmail.com>
2025-05-15 23:18:22 +0700

    cors

--- index.js
+++ index.js
@@ -47,8 +47,18 @@ module.exports = mongoose;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(',')
+  : [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'https://ships-customermoi.onrender.com'
+    ];
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://ships-customermoi.onrender.com'  ],
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
