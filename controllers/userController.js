e41178bc2bc7f6c9933ad9739bb3547b60fe860d
ngocluydepzai <vongocluy12345@gmail.com>
2025-05-15 14:52:20 +0700

    token

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ const authenticateToken = (roles) => (req, res, next) => {
   if (!token) {
     return res.status(401).json({ error: 'Không có token.' });
   }
-  jwt.verify(token, 'your_jwt_secret', (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ error: 'Token không hợp lệ.' });
     }
