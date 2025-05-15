8c4bd5822a8c5ca12fff3d25a23130db1783ddd2
ngocluydepzai <vongocluy12345@gmail.com>
2025-05-15 12:07:20 +0700

    Add socket.js

--- socket.js
+++ socket.js
@@ -7,7 +7,7 @@ const userSockets = {};
 function initSocket(server) {
   io = new Server(server, {
     cors: {
-      origin: ['http://localhost:5173', 'http://localhost:5174'],
+      origin: ['http://localhost:5173', 'http://localhost:5174', 'https://ships-admin.onrender.com'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
       credentials: true
     }
