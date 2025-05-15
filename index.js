4480f84dd7e068992b7f99ea0a935397055218ef
ngocluydepzai <vongocluy12345@gmail.com>
2025-05-15 17:22:09 +0700

    token

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ module.exports = mongoose;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002',     'https://ships-customermoi.onrender.com'  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
