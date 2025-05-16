274c2049e4bdd1fd57e0f24712d65b34b7701800
DESKTOP-G7KITEP\minh.nguyen <nndminh03@gmail.com>
2025-05-16 09:50:47 +0700

    comit dl

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -1,18 +1,6 @@
 const mongoose = require('mongoose');
 
-
 const chatMessageSchema = new mongoose.Schema({
-  orderId: { type: String, required: true },
-  sender: { type: String, required: true }, // user_id hoặc role
-  content: { type: String }, // nội dung tin nhắn
-  type: { type: String, default: 'text' }, // text, image, file, ...
-  fileUrl: { type: String }, // nếu là file
-  time: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('ChatMessage', chatMessageSchema); 
-
-const ChatMessageSchema = new mongoose.Schema({
   orderId: { type: String, required: true, index: true },
   sender: { type: String, required: true },
   content: { type: String },
@@ -21,5 +9,5 @@ const ChatMessageSchema = new mongoose.Schema({
   time: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('ChatMessage', ChatMessageSchema); 
+module.exports = mongoose.models.ChatMessage || mongoose.model('ChatMessage', chatMessageSchema); 
 
